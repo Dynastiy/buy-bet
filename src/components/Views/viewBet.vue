1ccb6b5f2fff15b4a381e1d6b1c6d776521c3367
Dynastiy <emmanuel00michael@gmail.com>
2020-03-05 14:35:55 +0100

    dist added

--- src/components/Views/viewBet.vue
+++ src/components/Views/viewBet.vue
@@ -6,11 +6,11 @@
 
     <div class="container mt-4">
         <div class="row mt-5" v-for="posts in data" :key="posts">
-            <div class="col-lg-9 col-sm-12 col-md-9">
-                <h4 class="text-white"> {{ posts.title }} Match: Arsenal - Chelsea</h4>
-                <p class="text-secondary"> {{ posts.body }}  Odd Type: Home Win </p>
+            <div class="col-lg-9 col-sm-12 col-md-12">
+                <h4 class="text-white"> {{ posts.home-team }} Match: Arsenal - Chelsea</h4>
+                <p class="text-secondary"> {{ posts.away-team }}  Odd Type: Home Win </p>
             </div>
-            <div class="col-lg-3 col-sm-12 col-md-9">
+            <div class="col-lg-3 col-sm-12 col-md-12">
                 <p class="text-secondary">Date: 02/10/2020</p>
                 <p class="text-secondary">Time: 09-04</p>
                 <h4 class="text-white">Winning: &#8358; 10, 000</h4>
@@ -35,7 +35,7 @@ export default {
 
   // Fetches data when the component is created.
   created () {    
-    axios.get(``).then(response => {
+    axios.get(`https://buybet-dc707.firebaseio.com/`).then(response => {
     // JSON responses are automatically parsed.
       this.data = response.data
     })
